ef6e7d3ec7825e062cee52beb8b5bf0f73d8c4e7
MixOpsVan <mixops1281@gmail.com>
2021-01-06 22:03:42 +0700

    Четвертое ДЗ.

--- src/geekbrians/homework/one/re.java
+++ src/geekbrians/homework/one/re.java
@@ -1,4 +1,15 @@
 package geekbrians.homework.one;
 
+import java.util.Scanner;
+
 public class re {
+    public static void Main (String [] args){
+        Number();
+    }
+    public static void Number(){
+        Scanner in = new Scanner(System.in);
+        int a, n = (int)(Math.random()*10);
+        System.out.println("Компьютер загадал число от 0 до 9, угадайте с 3 попыток");
+
+    }
 }
